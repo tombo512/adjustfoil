6374f56c0342b6bf2e6bc9bafbbe00986a9c552c
riekohirota <crista.p.p.j@gmail.com>
2015-05-22 02:58:56 +0900

    例外処理実装

--- ajustfoil.d
+++ ajustfoil.d
@@ -8,10 +8,14 @@ Ajustfoil Ver.0.0
 ファイル名，データが始まる行，データ形式，新しく作成するファイル名を入力する仕様．
 例外処理未実装．
 (R.Hirota)
+
+2015.5.22
+例外処理実装．
+(R.Hirota)
 ***************************
 
 ToDo
-- 例外処理
+- 例外処理 ...Done
 - データ形式自動読み取り
 - データ開始位置自動読み取り
 - ファイル名自動生成（？）
@@ -21,45 +25,61 @@ ToDo
 +/
 
 import std.stdio,
+	   std.file,
 	   std.string,
 	   std.conv;
 
 void main() {
-
-	write("\nFile open: ");
+	target: write("\nFile open: ");
 	auto filename = chomp(readln());	//define a file.
+	if(exists(filename)){		//check file name.
+		}else{
+			writeln("Error: file can not be opend");
+			target2: write("Continue? (y/n): ");
+			string ans;
+			ans = chomp(readln());
+			if(ans == "y"){
+				goto target;
+			}else if(ans == "n"){
+				return;
+			}else{
+				writeln("Error: unexpected input");
+				goto target2;
+			}
+		}
 	{
 		auto file = File(filename, "r");
+
 		write("\nStart line (0 origin): ");
-		int start = to!(int)(chomp(readln()));	//define the value start line.
+		int start = to!(int)(chomp(readln()));  //define the value start line.
+
 		write("\nValue form: ");
 		string form = chomp(readln());	//define the value form.
-
 		string foilname = file.readln();	//get the foil name.
 
 		for(int i = 0; i < start; i++){
-        	file.byLine.popFront();     // 1 line remove.
-    	}
+			file.byLine.popFront();     // empty lines remove.
+		}
 
-		float a = void, b = void;
+		float x = void, y = void;
 		float[] x_od, y_od;
-		int i = 0;
-		int border;
-		int flag = 0;
+		int count = 0,
+			border,
+			flag = 0;
 
-		while (file.readf(form, &a, &b)){
-			if(a == 1 && flag == 0){
-				border = i;
+		while (file.readf(form, &x, &y)){
+			if(x == 1 && flag == 0){
+				border = count;
 				flag = 1;
 			}
-			x_od ~= a;
-			y_od ~= b;					//ここクソだから直そうね！
+			x_od ~= x;
+			y_od ~= y;
 			file.byLine.popFront();     // 1 line remove.
-			i++;	
+			count++;	
 		}
 
 		float num1, num2;
-		for(i = 0; i < border/2; i++){
+		for(int i = 0; i < border/2; i++){
 			num1 = x_od[i];
 			x_od[i] = x_od[border - i];
 			x_od[border - i] = num1;
@@ -79,22 +99,24 @@ void main() {
 			for(k = 0; k < x_od.length; k++){
 				file2.writef(" %5f\t%5f\n", x_od[k], y_od[k]);	
 			}
+
+		writeln("\n...done \n");
+
 		}
-	}	
+	}
+
+	target3: write("Continue? (y/n): ");
+	string ans;
+	ans = chomp(readln());
+	if(ans == "y"){
+		goto target;
+	}else if(ans == "n"){
+		return;
+	}else{
+		write("Error: unexpected input");
+		goto target3;
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
